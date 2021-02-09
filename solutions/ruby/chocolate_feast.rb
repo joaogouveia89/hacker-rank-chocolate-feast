bbc2b38c1e65ed6b5df81f095be07a8d4bd78162
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-02-09 15:14:38 +0000

    removed useless coments from ruby solution

--- solutions/ruby/chocolate_feast.rb
+++ solutions/ruby/chocolate_feast.rb
@@ -1,9 +1,5 @@
 def chocolate_feast n, c, m
 
-	#inputs with problems
-	# 16779 1 5886 ----- getting 16782 expected = 16781
-	# 15542 10 750 ---- getting 1557 expected = 1556
-	# 857 2 43 ---- getting 486 expected = 438
 	wrappers = (n/c).floor
 	ate = wrappers
 
